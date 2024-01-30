ec0cb7f856c29388f1d0ac3cc485249aad671231
Zam <samuel.kodehode@gmail.com>
2024-01-30 08:48:59 +0100

    Structure, Array in separate file, added classes

--- app.js
+++ app.js
@@ -1,148 +1,38 @@
-const topSellers = [
-  {
-    title: "Grand Theft Auto V",
-    developer: "Rockstar Games",
-    releaseYear: 2013,
-    categories: ["Action", "Adventure", "RPG"],
-    description:
-      "Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the seventh main entry in the Grand Theft Auto series, set within the fictional state of San Andreas. The single-player story follows three protagonists—retired bank robber Michael De Santa, street gangster Franklin Clinton, and drug dealer Trevor Philips—and their attempts to commit heists while under pressure from a corrupt government agency and powerful criminals.",
-    platforms: ["PC", "Playstation", "X-Box"],
-    logo: "gtav-logo.jpg",
-  },
-  {
-    title: "Minecraft",
-    developer: "Mojang",
-    releaseYear: 2011,
-    categories: ["Sandbox", "Survival"],
-    description:
-      "Minecraft is a sandbox video game developed and published by Mojang. In Minecraft, players can build and explore virtual worlds made up of blocks. The game has various modes, including survival mode where players must acquire resources and maintain health, and creative mode where players have unlimited resources and can create elaborate structures.",
+import { topSellers } from './resorces/topSellers.js'
 
-    platforms: ["PC", "Playstation", "X-Box", "Nintendo"],
-    logo: "minecraft-logo.jpg",
-  },
-  {
-    title: "Tetris",
-    developer: "Various",
-    releaseYear: 1984,
-    categories: ["Puzzle"],
-    description:
-      "Tetris is a tile-matching puzzle video game originally designed and programmed by Alexey Pajitnov. In Tetris, geometric shapes called Tetriminos fall from the top of the screen, and the player must rotate and arrange them to create horizontal lines without any gaps. When a line is completed, it disappears, and the player earns points.",
+const gamesPostersContainer = document.getElementById('games-posters-container')
+const gamesList = document.createElement('ol')
 
-    platforms: ["Various"],
-    logo: "tetris-logo.jpg",
-  },
-  {
-    title: "Wii Sports",
-    developer: "Nintendo",
-    releaseYear: 2006,
-    categories: ["Sports"],
-    description:
-      "Wii Sports is a sports video game developed and published by Nintendo. It was designed as a launch title for the Nintendo Wii console and features a collection of five sports simulations, including tennis, baseball, bowling, golf, and boxing. The game utilizes the Wii Remote controller's motion-sensing capabilities for immersive gameplay.",
+gamesPostersContainer.append(gamesList)
+document.body.append(gamesPostersContainer)
 
-    platforms: ["Nintendo Wii"],
-    logo: "wii-sports-logo.png",
-  },
-  {
-    title: "PlayerUnknown's Battlegrounds (PUBG)",
-    developer: "PUBG Corporation",
-    releaseYear: 2017,
-    categories: ["Battle Royale"],
-    description:
-      "PlayerUnknown's Battlegrounds (PUBG) is an online multiplayer battle royale game developed and published by PUBG Corporation. In PUBG, up to 100 players parachute onto an island, scavenge for weapons and equipment, and fight to be the last person or team standing. The game's map size gradually decreases over time, forcing players into closer proximity and increasing the intensity of the battles.",
+topSellers.forEach((item) => {
+	const gameElem = document.createElement('li')
+	const gameLogo = document.createElement('img')
+	const gameTitle = document.createElement('h2')
+	const gameDeveloper = document.createElement('h3')
+	const gamePlatforms = document.createElement('p')
+	const gameCategories = document.createElement('p')
+	const gameReleaseYear = document.createElement('p')
+	const gameDescription = document.createElement('p')
 
-    platforms: ["PC", "Playstation", "X-Box", "Mobile"],
-    logo: "pubg-logo.jpg",
-  },
-  {
-    title: "Super Mario Bros.",
-    developer: "Nintendo",
-    releaseYear: 1985,
-    categories: ["Platformer"],
-    description:
-      "Super Mario Bros. is a platform game developed and published by Nintendo. It follows Mario and Luigi as they embark on a quest to rescue Princess Toadstool from the villainous King Bowser. The game is renowned for its side-scrolling gameplay, power-ups like the Super Mushroom and Fire Flower, and the iconic characters that have become staples of the Nintendo franchise.",
+	gameLogo.className = 'icons-class'
+	gameTitle.className = 'game-title'
+	gameElem.className = 'game-container'
+	gameDeveloper.className = 'game-Developer'
+	gamePlatforms.className = 'game-Platforms'
+	gameCategories.className = 'game-Categories'
+	gameReleaseYear.className = 'game-ReleaseYear'
+	gameDescription.className = 'game-Description'
 
-    platforms: ["Nintendo Entertainment System (NES)"],
-    logo: "super-mario-logo.png",
-  },
-  {
-    title: "The Legend of Zelda: Breath of the Wild",
-    developer: "Nintendo",
-    releaseYear: 2017,
-    categories: ["Action", "Adventure"],
-    description:
-      "The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo. Set in a vast open world, players control Link as he explores the kingdom of Hyrule, battles enemies, solves puzzles, and embarks on a quest to defeat the ancient evil Calamity Ganon. The game is praised for its immersive gameplay and attention to detail.",
+	gameTitle.textContent = item.title
+	gameLogo.src = `logos/${item.logo}`
+	gameDeveloper.textContent = item.developer
+	gameReleaseYear.textContent = item.releaseYear
+	gameDescription.textContent = item.description
+	gamePlatforms.textContent = item.platforms.join(' , ')
+	gameCategories.textContent = `#${item.categories.join(' #')}`
 
-    platforms: ["Nintendo Switch", "Wii U"],
-    logo: "zelda-botw-logo.jpg",
-  },
-  {
-    title: "Garry's Mod",
-    developer: "Facepunch Studios",
-    releaseYear: 2004,
-    categories: ["Sandbox"],
-    description:
-      "Garry's Mod is a sandbox game developed by Facepunch Studios. It allows players to manipulate objects and experiment with physics in a virtual environment. Garry's Mod has a diverse and active community that creates a wide range of user-generated content, including game modes, maps, and modifications using the Source engine.",
-
-    platforms: ["PC"],
-    logo: "garrys-mod-logo.jpg",
-  },
-  {
-    title: "Red Dead Redemption 2",
-    developer: "Rockstar Games",
-    releaseYear: 2018,
-    categories: ["Action", "Adventure"],
-    description:
-      "Red Dead Redemption 2 is an action-adventure game developed by Rockstar Games. Set in the late 1800s, the game follows Arthur Morgan, a member of the Van der Linde gang, as he navigates the changing American frontier. The game features an expansive open world, immersive storytelling, and a mix of action, exploration, and interaction with a rich cast of characters.",
-
-    platforms: ["Playstation", "X-Box", "PC"],
-    logo: "red-dead-logo.jpg",
-  },
-  {
-    title: "Mario Kart 8 Deluxe",
-    developer: "Nintendo",
-    releaseYear: 2017,
-    categories: ["Racing"],
-    description:
-      "Mario Kart 8 Deluxe is a kart racing game developed by Nintendo. It is the enhanced version of Mario Kart 8 for the Nintendo Switch console. Players race against each other on various tracks, using power-ups and items to gain an advantage. The game features a diverse roster of characters from the Mario series and introduces new elements such as anti-gravity racing.",
-
-    platforms: ["Nintendo Switch"],
-    logo: "mario-kart-logo.jpg",
-  },
-];
-const gamesPostersContainer = document.getElementById(
-  "games-posters-container"
-);
-document.body.append(gamesPostersContainer);
-const gamesList = document.createElement("ol");
-gamesPostersContainer.append(gamesList);
-for (let i = 0; i < topSellers.length; i++) {
-  const gameElem = document.createElement("li");
-
-  const gameTitle = document.createElement("h2");
-  const gameDeveloper = document.createElement("h3");
-  const gameReleaseYear = document.createElement("p");
-  const gameCategories = document.createElement("p");
-  const gameDescription = document.createElement("p");
-  const gameplatforms = document.createElement("p");
-  const gameLogo = document.createElement("img");
-  gameLogo.classList.add("icons-class");
-
-  gameTitle.textContent = topSellers[i].title;
-  gameDeveloper.textContent = topSellers[i].developer;
-  gameReleaseYear.textContent = topSellers[i].releaseYear;
-  gameCategories.textContent = topSellers[i].categories.join(" , ");
-  gameDescription.textContent = topSellers[i].description;
-  gameplatforms.textContent = topSellers[i].platforms.join(" , ");
-  gameLogo.src = `logos/${topSellers[i].logo}`;
-
-  gameElem.append(
-    gameTitle,
-    gameDeveloper,
-    gameReleaseYear,
-    gameCategories,
-    gameDescription,
-    gameplatforms,
-    gameLogo
-  );
-  gamesList.append(gameElem);
-}
+	gameElem.append(gameTitle, gameDeveloper, gameReleaseYear, gameCategories, gameDescription, gamePlatforms, gameLogo)
+	gamesList.append(gameElem)
+})
